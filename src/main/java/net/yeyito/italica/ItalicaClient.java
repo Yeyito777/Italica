777f33be04d82acb06687ef055b44b035d64abb6
Yeyito777 <alinerob31@gmail.com>
2022-11-19 18:23:00 -0500

    Italica from ExampleMod

---
+++ src/main/java/net/yeyito/italica/ItalicaClient.java
@@ -0,0 +1,10 @@
+package net.yeyito.italica;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItalicaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
